a692b7df545cc8a6455a5148c15c4e9d34a211cd
Pavel Shevchuk <pavels-k@mail.ru>
2021-05-08 23:26:44 +0300

    Removed relative path

--- Learning_Spark/3_4_rdd.py
+++ Learning_Spark/3_4_rdd.py
@@ -7,8 +7,10 @@ from pyspark.sql import SQLContext
 spark = SparkContext.getOrCreate(SparkConf())
 sqlContext = SQLContext(spark)
 
-table = sqlContext.read.csv('C:\\Users\\Павел\\Desktop\\Спарк_Практика\\Моё\\test_table.csv', header="true", sep = ';')
+
+
+table = sqlContext.read.csv('test_table.csv', header="true", sep = ';')
 table.show()
 
-lines = sqlContext.read.text("C:\\Users\\Павел\\Desktop\\Спарк_Практика\\Моё\\test_text.txt")
+lines = sqlContext.read.text("test_text.txt")
 lines.show()
